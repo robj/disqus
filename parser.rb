d0e96e2cc17bd79cb96b8f5fcb3d8e99f2d07e57
robj <robert.jennings@gmail.com>
2015-10-13 17:29:59 -0700

    add formatting including iso8601 time conversion

--- parser.rb
+++ parser.rb
@@ -3,6 +3,8 @@
 require 'apachelogregex'
 require 'pp'
 require 'json'
+require 'date'
+require 'time'
 
 logfile = ARGV[0]
 
@@ -10,52 +12,73 @@ logfile = ARGV[0]
 
 #http://httpd.apache.org/docs/2.2/logs.html
 format = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
-#format = '%h %l %u %t \" %m %U%q %H \" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
+
+
+  #   format = '%h %l %u %t \" %m %U%q %H \" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'
+  #   hash['method'] = tokenized_line["%m"]
+  #   hash['url'] = tokenized_line["%U%q"]
+  #   hash['protocol'] = tokenized_line["%H"]
 
 
 
 @parser = ApacheLogRegex.new(format)
 @line_hashes = []
 
-def line_to_hash(line)
+def line_to_formatted_hash(line)
 
     tokenized_line =  @parser.parse(line)
-   # puts tokenized_line['\"%{Referer}i\']
-  #  pp tokenized_line
 
-     request = tokenized_line["%r"]
-     method = request.split(' ')[0]
-     url = request.split(' ')[1]
-     protocol =  request.split(' ')[2]
+    hash = Hash.new
 
-     hash = Hash.new
+    #split request into method, url, protocol
 
-     hash['remote'] = tokenized_line["%h"]
-     hash['identity'] = tokenized_line["%l"]
-     hash['user'] = tokenized_line["%u"]
-     hash['date'] = tokenized_line["%t"].gsub('[','').gsub(']','')
-     hash['status'] = tokenized_line["%>s"].to_i
-     hash['bytes'] = tokenized_line["%b"].to_i
-     hash['referer'] = tokenized_line["%{Referer}i"]
-     hash['user-agent'] = tokenized_line["%{User-Agent}i"]
+    request = tokenized_line["%r"]
+    method = request.split(' ')[0]
+    url = request.split(' ')[1]
+    protocol =  request.split(' ')[2]
 
-     hash['method'] = method
-     hash['url'] = url
-     hash['protocol'] = protocol
+    hash['method'] = method
+    hash['url'] = url
+    hash['protocol'] = protocol
+
+    #fields that can be copied directly
+
+    hash['remote'] = tokenized_line["%h"]
+    hash['identity'] = tokenized_line["%l"]
+    hash['user'] = tokenized_line["%u"]
+    hash['referer'] = tokenized_line["%{Referer}i"]
+    hash['user-agent'] = tokenized_line["%{User-Agent}i"]
 
 
-  #   hash['method'] = tokenized_line["%m"]
-  #   hash['url'] = tokenized_line["%U%q"]
-  #   hash['protocol'] = tokenized_line["%H"]
+    #fields that require type conversion
 
-     @line_hashes << hash
+    hash['status'] = tokenized_line["%>s"].to_i
+    hash['bytes'] = tokenized_line["%b"].to_i
+
+
+
+   # convert apache date string to UTC ISO 8601 time
+
+    apache_date_string = tokenized_line["%t"]
+    puts apache_date_string
+    date_time = DateTime.strptime( apache_date_string, "[%d/%b/%Y:%H:%M:%S %Z]")
+    iso8601_time = date_time.to_time.utc.iso8601
+
+    hash['date'] = iso8601_time 
+
+
+
+    hash
+
+
 
 end
 
 
 File.foreach logfile do |line|
 
-      line_to_hash(line)
+      formatted_hash = line_to_formatted_hash(line)
+      @line_hashes << formatted_hash
 
 end
 
